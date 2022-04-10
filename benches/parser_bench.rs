75abc27d48243a8a2cbc867c08c2bff80e1d2d0a
Benjamin Pinter <ben.david.pinter@icloud.com>
2022-04-09 22:26:29 -0400

    Clippy Fixes

--- benches/parser_bench.rs
+++ benches/parser_bench.rs
@@ -1,47 +1,47 @@
 extern crate sql_parser;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 fn no_select_star() {
     let _violations = sql_parser::lint_files(&["noselectstar.sql"]);
 }
 pub fn bench_no_select_star(c: &mut Criterion) {
-    c.bench_function("No SELECT *", |b| b.iter(|| no_select_star()));
+    c.bench_function("No SELECT *", |b| b.iter(no_select_star));
 }
 
 fn no_open_trans_at_end_of_file() {
     let _violations = sql_parser::lint_files(&["noopentran.sql"]);
 }
 pub fn bench_no_open_trans_at_end_of_file(c: &mut Criterion) {
-    c.bench_function("Unclosed TRAN", |b| b.iter(|| no_open_trans_at_end_of_file()));
+    c.bench_function("Unclosed TRAN", |b| b.iter(no_open_trans_at_end_of_file));
 }
 
 fn no_nolock() {
     let _violations = sql_parser::lint_files(&["nonolock.sql"]);
 }
 pub fn bench_no_nolock(c: &mut Criterion) {
-    c.bench_function("No NOLOCK", |b| b.iter(|| no_nolock()));
+    c.bench_function("No NOLOCK", |b| b.iter(no_nolock));
 }
 
 fn no_select_in_tran() {
     let _violations = sql_parser::lint_files(&["noselectintran.sql"]);
 }
 pub fn bench_no_select_in_tran(c: &mut Criterion) {
-    c.bench_function("No SELECT in TRAN", |b| b.iter(|| no_select_in_tran()));
+    c.bench_function("No SELECT in TRAN", |b| b.iter(no_select_in_tran));
 }
 
 fn no_funcs_in_where_clause() {
     let _violations = sql_parser::lint_files(&["nofunctionsinwhere.sql"]);
 }
 pub fn bench_no_funcs_in_where_clause(c: &mut Criterion) {
-    c.bench_function("No f() in WHERE", |b| b.iter(|| no_funcs_in_where_clause()));
+    c.bench_function("No f() in WHERE", |b| b.iter(no_funcs_in_where_clause));
 }
 
 fn no_declare_in_tran() {
     let _violations = sql_parser::lint_files(&["nodeclareintran.sql"]);
 }
 pub fn bench_no_declare_in_tran(c: &mut Criterion) {
-    c.bench_function("No DECLARE in TRAN", |b| b.iter(|| no_declare_in_tran()));
+    c.bench_function("No DECLARE in TRAN", |b| b.iter(no_declare_in_tran));
 }
 
 
